a71603997a002906220f9b66835769d71973b2d7
VivyaCC <vccsya@gmail.com>
2023-04-10 04:58:10 +0200

    .

--- build.cmd
+++ build.cmd
@@ -1,1 +1,5 @@
-yarn --force --ignore-engines && yarn run gulp vscode-win32-x64 --openssl-legacy-provider
+@echo off
+setlocal
+call yarn config set ignore-engines true
+call yarn --force
+call yarn run gulp vscode-win32-x64 --openssl-legacy-provider
